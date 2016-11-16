f53e38cb80706e8e4e85a400758079b19bd8da20
Dylan de Wolff <jaxwolff@avans.nl>
2016-11-16 13:10:53 +0100

    Heyo, second commit.
    This commit is just to say that we at RPG-C#-INC© uphold certain values and ideas, and those include commiting updates to applications with an extensive description included, a single world describing a commit so shortly is not sufficient. Please pay attention to this in the future other co-worker
    
    Greetings, master developer DutchDylan

--- Project RPG Game/Project RPG Game/Forms/WorldForm.Designer.cs
+++ Project RPG Game/Project RPG Game/Forms/WorldForm.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // WorldForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 304);
+            this.Name = "WorldForm";
             this.Text = "WorldForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
